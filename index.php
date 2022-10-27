da0f3e8c1132540de69e0ce6d3ebf2492cc06a20
Ventsislav <vncnklv0@gmail.com>
2022-10-27 12:42:17 +0300

    initial commit

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+    </head>
+    <body>
+        <div>
+            <a href="contacts.php">Контакти</a>
+        </div>
+        Здравей :)
+    </body>
+</html>
